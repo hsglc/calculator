4ed93afc87ec7c6f1c121065becfdec9fabb59e1
huseyin gulcu <huseyingulcuu@gmail.com>
2021-01-07 16:22:46 +0300

    has edited the readMe.md file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# calculator
+# Pure JavaScript Calculator
+This has built by using pure javascript without any frameworks.
+
+## Usage
+Here's [a little demo](https://codepen.io/pen/) for project.
+
+## Appearance
+
+![GIF](https://media.giphy.com/media/hDrzAlN7W3YpW2Flxn/giphy.gif)
---
+++ calc.js
@@ -0,0 +1,111 @@
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  operator: null,
+  waitingForSecondOperand: false,
+};
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (firstOperand == null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = `${parseFloat(result.toFixed(4))}`;
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = "0.";
+    calculator.waitingForSecondOperand = false;
+    return;
+  }
+
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+      //console.log(calculator.displayValue);
+  }
+}
+function init() {
+  const keys = document.querySelector(".calculator-keys");
+  keys.addEventListener("click", (event) => {
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches("button")) {
+      return;
+    }
+
+    switch (value) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+      case "=":
+        handleOperator(value);
+        break;
+      case ".":
+        inputDecimal(value);
+        break;
+      case "all-clear":
+        resetCalculator();
+        break;
+      default:
+        if (Number.isInteger(parseFloat(value))) {
+          inputDigit(value);
+        }
+    }
+
+    updateDisplay();
+  });
+}
+
+function resetCalculator() {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+function updateDisplay() {
+  const display = document.querySelector(".calculator-screen");
+  display.value = calculator.displayValue;
+}
+
+init();
